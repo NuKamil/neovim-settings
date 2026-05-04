54ed13b0cf4d007aea30146e5997b5427a8e7f26
Kamil Nurkowski <kamill1983@gmail.com>
2026-05-04 14:18:33 +0200

    Nvim with Neovide, opencode fix

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,24 +1,31 @@
 return {
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('rose-pine').setup {
-        variant = 'moon',
-        dark_variant = 'moon',
-        disable_background = true,
-      }
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			if vim.g.neovide then
+				vim.g.neovide_opacity = 0.92
+				vim.g.neovide_normal_opacity = 0.92
+			end
+			require("rose-pine").setup({
+				variant = "main",
+				dark_variant = "main",
+				extend_background_behind_borders = true,
+				styles = {
+					transparency = false,
+				},
+			})
 
-      vim.cmd.colorscheme 'rose-pine'
+			vim.cmd.colorscheme("rose-pine")
 
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'FoldColumn', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
-    end,
-  },
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+		end,
+	},
 }
--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -1,27 +1,40 @@
 return {
-  {
-    'nickjvandyke/opencode.nvim',
-    version = '*',
-    config = function()
-      vim.g.opencode_opts = {}
+	{
+		"nickjvandyke/opencode.nvim",
+		version = "*",
+		config = function()
+			vim.g.opencode_opts = {}
+			vim.o.autoread = true
 
-      local oc = require 'opencode'
+			local oc = require("opencode")
 
-      vim.keymap.set({ 'n', 't' }, '<leader>oo', function()
-        oc.toggle()
-      end, { desc = 'OpenCode: toggle' })
+			vim.keymap.set({ "n", "t" }, "<leader>oo", function()
+				oc.toggle()
+			end, { desc = "OpenCode: toggle panel" })
 
-      vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
-        oc.ask('@this: ', { submit = true })
-      end, { desc = 'OpenCode: ask @this' })
+			vim.keymap.set({ "n", "x" }, "<leader>oe", function()
+				return oc.operator("Explain @this", { submit = true })
+			end, { expr = true, desc = "OpenCode: explain selection" })
 
-      vim.keymap.set({ 'n', 'x' }, '<leader>os', function()
-        oc.select()
-      end, { desc = 'OpenCode: select' })
+			vim.keymap.set("n", "<leader>oee", function()
+				return oc.operator("Explain @this", { submit = true }) .. "_"
+			end, { expr = true, desc = "OpenCode: explain current line" })
 
-      vim.keymap.set('n', '<leader>on', function()
-        oc.command 'session.new'
-      end, { desc = 'OpenCode: new session' })
-    end,
-  },
+			vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+				return oc.operator("@this ")
+			end, { expr = true, desc = "OpenCode: add selection to prompt" })
+
+			vim.keymap.set("n", "<leader>oaa", function()
+				return oc.operator("@this ") .. "_"
+			end, { expr = true, desc = "OpenCode: add current line to prompt" })
+
+			vim.keymap.set({ "n", "x" }, "<leader>os", function()
+				oc.select()
+			end, { desc = "OpenCode: select action" })
+
+			vim.keymap.set("n", "<leader>on", function()
+				oc.command("session.new")
+			end, { desc = "OpenCode: new session" })
+		end,
+	},
 }
